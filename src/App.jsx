d3219b7cf0432b474bcab3115ed9288a53a70fa0
paulocaetano96 <paulocaetano96@gmail.com>
2023-03-10 16:04:29 +0000

    app.js update

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<Home />} />
+
       </Routes>
     </div>
   )
