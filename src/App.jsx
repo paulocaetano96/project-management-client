a35a884eb362b0c68629a260ec3c41e240f453d6
paulocaetano96 <paulocaetano96@gmail.com>
2023-03-10 12:15:56 +0000

    package installation

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <div>
-      {/* coommentttt */}
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
