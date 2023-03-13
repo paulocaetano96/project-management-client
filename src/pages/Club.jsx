2d3898a8ed8708ebfea776481d02c62bd05e5d17
paulocaetano96 <paulocaetano96@gmail.com>
2023-03-13 16:01:34 +0000

    club services update

--- src/pages/Club.jsx
+++ src/pages/Club.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
+import clubService from '../services/club.services';
 
 
 function Club() {
-  return (
-    <div>ClubDetails</div>
-  )
-}
 
-export default Club
+  const [club, setClub] = useState('');
+  const [name, setName] = useState('');
+
+
+
+
+  return (
+    <div>
+      <p>Club Name: </p>
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default Club;
